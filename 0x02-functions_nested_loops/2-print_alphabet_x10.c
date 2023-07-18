96b6e37037c00bb72a461bc54066c81ec8d554dc
Your Name <sobukwelutho@gmail.com>
2023-07-18 14:59:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-	char ch;
+	int j;
 	int i;
 
 	i = '0';
 
 	while (i < 10)
 	{
-		ch = 'z';
+		j = 'z';
 
-		while (ch <= 'z')
+		while (j <= 'z')
 		{
-			_putchar(ch);
+			_putchar(j);
 			ch++;
 		}
 		_putchar('\n');
