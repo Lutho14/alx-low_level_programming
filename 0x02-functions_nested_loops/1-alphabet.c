182e8db72d171d5f7a19623467a611fc65df344f
Your Name <sobukwelutho@gmail.com>
2023-07-18 14:24:06 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - print the alphabet
- * followed by new line
+ * print_alphabet - a function that print the alphabet
+ *
  * Return: always 0
  */
 
 void print_alphabet(void)
 {
-	int letter;
+	int i;
 
-	for (letter = 'a', letter <= 'z', letter++)
+	for (i = 'a', i <= 'z', i++)
 	{
-		_putchar(letter);
+		_putchar(i);
 	}
 
 	_putchar('\n');
