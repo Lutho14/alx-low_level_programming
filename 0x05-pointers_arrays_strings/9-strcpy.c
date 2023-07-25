ffb843c1910f66746c30a939d33e5c64abae03d4
Your Name <sobukwelutho@gmail.com>
2023-07-25 15:35:23 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (*(src + 1) != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
-	}
-	for (; j < i; j++)
-	{
-		dest[j] = src[j];
+		dest[i] = src[i];
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
