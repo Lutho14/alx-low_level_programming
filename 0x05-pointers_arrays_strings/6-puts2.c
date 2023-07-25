0a1ba02c520ba9f6489c45809033353b0816d43f
Your Name <sobukwelutho@gmail.com>
2023-07-25 11:02:05 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2: prints every other character of a string, starting from the first character
+ * puts2: prints every other character of a string
  * @str: string to be checked
  */
 
