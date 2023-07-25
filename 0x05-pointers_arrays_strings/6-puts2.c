67dab955b79c9e73c094d912ee094e9fabf0a51f
Your Name <sobukwelutho@gmail.com>
2023-07-25 15:00:58 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,19 @@
 #include "main.h"
 
 /**
- * puts2: prints every other character of a string
+ * puts2 - prints every other character of a string from first character
  * @str: string to be checked
  */
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
+		if (i % 2 == 0)
 		{
-			putchar(str[0]);
+			putchar(str[i]);
 		}
 	}
 	putchar('\n');
