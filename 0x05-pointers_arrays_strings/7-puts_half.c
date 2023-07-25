bac3cbab020ec9830b910c8695dbbefc52e8c232
Your Name <sobukwelutho@gmail.com>
2023-07-25 15:18:18 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,4 +8,18 @@
 
 oid puts_half(char *str)
 {
+	int len = 0;
+	int half, i;
 
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	half = (len - 1) / 2;
+
+	for (i = (half + 1); i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
