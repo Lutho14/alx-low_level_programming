0dff98b4bad7addc45ac05211adf098dc9c15397
Your Name <sobukwelutho@gmail.com>
2023-07-27 15:51:46 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_array - a function that reverses the content of an array of integers
+ * reverse_array - reverses array of integers
  * @a: array
  * @n: number of elements of array
  * Return: void
