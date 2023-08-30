7698559ad7325b762a979c0f592102e234c35e3a
Lutho14 <sobukwelutho@gmail.com>
2023-08-29 17:01:26 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num_nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d/n", h->n);
 		h = h->next;
-		num_nodes++;
+		num++;
 	}
-	return (num_nodes);
+	return (num);
 
 }
